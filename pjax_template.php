d438e32c1cd790c6c5e1764b101974dcca8132fb
Miljenko Barbir <miljenko.barbir@gmail.com>
2011-10-13 22:36:39 +0200

    title tag added to pjax template

--- pjax_template.php
+++ pjax_template.php
@@ -3,7 +3,23 @@
  * A Template for displaying pjax-loaded content.
  */
 ?>
+<title><?php
+	/*
+	 * Print the <title> tag based on what is being viewed.
+	 */
+	global $page, $paged;
 
+	wp_title( '|', true, 'right' );
+
+	// Add the blog name.
+	bloginfo( 'name' );
+
+	// Add the blog description for the home/front page.
+	$site_description = get_bloginfo( 'description', 'display' );
+	if ( $site_description && ( is_home() || is_front_page() ) )
+		echo " | $site_description";
+
+?></title> 
 <div id="primary">
 	<div id="content" role="main" class="blablabl">
 
